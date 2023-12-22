93768e22efe4af1c9ea922c21133f020887cac13
Livia Zaharia <the.great.white.ai@gmail.com>
2023-12-22 22:18:37 +0200

    some subdivision

--- 0-1 clustering for sorting RESNET BASE-sequential clustering.py
+++ 0-1 clustering for sorting RESNET BASE-sequential clustering.py
@@ -14,10 +14,6 @@ import tensorflow as tf
 from sklearn.cluster import DBSCAN
 from sklearn.neighbors import NearestNeighbors
 
-#####################################
-# Defining a skeleton for our       #
-# DataFrame                         #
-#####################################
 
 sk_struct = {
     'photo_name' : [],
@@ -25,22 +21,11 @@ sk_struct = {
     }
 
 
-#######################################################################################
-#      The Approach is to apply transfer learning                                     #
-#######################################################################################
-
-
 
 MyModel=tf.keras.applications.ResNet101(
     include_top = False, weights='imagenet', pooling='avg')
 
-"""
-Some topics about what happened here. A model has been definied in the Resnet structure from Keras
-It is a sequential model and has a number of layers in it
-At the moment it has only recieved the input from the resnet structure
-"""
-# freezing weights for 1st layer
-#MyModel.layers[0].trainable = False
+
 MyModel.trainable=False
 
 MyModel.compile(optimizer=tf.keras.optimizers.Adam())
@@ -58,9 +43,12 @@ Here ends the model as defined by resnet
 
 
 
-### Now defining dataloading Function
 
 def LoadDataAndDoEssentials(path, h, w):
+    """
+    Part2/2 of the basic setup used to process the images. It is called by part 1/2
+    """
+    
     img = cv2.imread(str(path))
     img = cv2.resize(img, (h, w))
     
@@ -68,31 +56,20 @@ def LoadDataAndDoEssentials(path, h, w):
     img = np.expand_dims(img, 0)
 
     img = tf.keras.applications.resnet50.preprocess_input(img)
-
-        
+   
     extractedFeatures = MyModel.predict(img)
-    
-    """
-    print("AICI SUNT IMG DVS")
-    print(type(img))
-    print(img)
-    print(img.shape)
-    print("si aici xtracted features")
-    print(type(extractedFeatures))
-    print(extractedFeatures)
-    print(extractedFeatures.shape)
-    """
-    
+        
     extractedFeatures = np.array(extractedFeatures)
 
     #THIS IS THE IMPORTANT ROW IN THIS PART
     sk_struct['flattenPhoto'].append(extractedFeatures.flatten())
-
-
     
-### with this all done lets write the iterrrative loop
 
 def ReadAndStoreMyImages(path):
+    """
+    Part1/2 of the basic setup used to process the images. 
+    It is called from main at the moment and sets up the looping
+    """
 
     filenames_only = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
     i=0
@@ -111,7 +88,33 @@ def ReadAndStoreMyImages(path):
         '''
         
         
+def filter_the_array(x_val, filter_val, filter_list):
+    return np.fromiter((x for (y,x) in enumerate(x_val) if filter_list[y]==filter_val),
+                      dtype = 'object') 
+
+
+
+def compute_dbscan_labels(object_x):
+    new_x = np.vstack(object_x[:]).astype(np.float64)
     
+    # Compute the KNN distance
+    knn.fit(new_x)
+    new_distances, new_indices = knn.kneighbors(new_x)
+
+    new_distances = np.sort(new_distances, axis=0)
+    new_distances = new_distances[:,1]
+
+    new_eps=new_distances.mean()
+
+    # Apply DBSCAN
+    new_dbscan = DBSCAN(eps=new_eps, min_samples=5)
+    new_dbscan.fit(new_x)
+
+    new_labels = new_dbscan.labels_
+    new_unique_labels=set(new_labels)
+    print(new_unique_labels)
+ 
+
 
 
 
@@ -131,13 +134,10 @@ ReadAndStoreMyImages(TRAINING_DATA_DIR)
 
 X = np.array(sk_struct['flattenPhoto'], dtype = 'float64')
 
-"""
-Basically it gets an array of information for every pixel out there
-"""
 
 print("shape of X")
 print (X.shape)
-# #####################################
+
 # Compute the KNN distance
 knn = NearestNeighbors(n_neighbors=2)
 knn.fit(X)
@@ -146,7 +146,7 @@ print("shape of distances")
 print(distances.shape)
 print ("shape of indices")
 print(indices.shape)
-array_dist=knn.kneighbors_graph(X).toarray()
+
 distances = np.sort(distances, axis=0)
 distances = distances[:,1]
 print("shape of distances2")
@@ -162,15 +162,35 @@ print("shape of dbscan")
 print(dbscan.labels_.shape)
 
 labels = dbscan.labels_
+unique_labels=set(labels)
 
-print("shape of levels")
-print(set(labels))
-print(1 if -1 in set(labels) else 0)
+
 # Count the number of clusters
-num_clusters = len(set(labels)) - (1 if -1 in set(labels) else 0)
+num_clusters = len(unique_labels) - (1 if -1 in unique_labels else 0)
 print('Number of clusters:', num_clusters)
 
-for label in labels:
+
+
+for label in unique_labels:
+    new_x=filter_the_array(X,label,labels)
+    compute_dbscan_labels(new_x)
+
+"""
+
+print("new-x dtype",float_arr.dtype)
+print("new-x shape", float_arr.shape)
+    
+print("new-x element dytpe", type(float_arr[0]))
+print(len(float_arr[0]))
+print(float_arr[0])
+
+print("x dtype",X.dtype)
+print("x shape", X.shape)
+print("x element dytpe", type(X[0]))
+print(len(X[0]))
+print(X[0])
+
+for label in unique_labels:
     new_path=data_path/str(label)
     if new_path not in data_path.glob("*"):
         os.mkdir(new_path)
@@ -181,3 +201,4 @@ for filename, label in zip(filenames, labels):
     path=data_path/str(label)
     shutil.copy(filename,path)
 
+"""
