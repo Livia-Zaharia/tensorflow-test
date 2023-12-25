f4280f41b4734cff1029285c3cd1df13dfdf4f36
Livia Zaharia <the.great.white.ai@gmail.com>
2023-12-25 17:01:25 +0200

    readme added

---
+++ 0-1 clustering for sorting RESNET BASE-sequential clustering+high res.py
@@ -0,0 +1,222 @@
+
+import sklearn
+from pathlib import Path
+import os
+import glob
+import shutil
+import sys
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+from sklearn.cluster import DBSCAN
+from sklearn.neighbors import NearestNeighbors
+
+
+sk_struct = {
+    'photo_name' : [],
+    'flattenPhoto' : []
+    }
+
+
+
+MyModel=tf.keras.applications.ResNet101(
+    include_top = False, weights='imagenet',pooling='avg',input_shape=(1000,1000,3))
+
+
+MyModel.trainable=False
+
+MyModel.compile(optimizer=tf.keras.optimizers.Adam())
+
+#MyModel.trainable=True
+
+#MyModel.layers[-1].trainable = False
+#MyModel.compile(optimizer=tf.keras.optimizers.Adam())
+
+
+"""
+Here ends the model as defined by resnet
+"""
+
+
+
+
+
+def LoadDataAndDoEssentials(path, h, w):
+    """
+    Part2/2 of the basic setup used to process the images. It is called by part 1/2
+    """
+    
+    img = cv2.imread(str(path))
+    img = cv2.resize(img, (h, w))
+    
+    ## Expanding image dims so this represents 1 sample
+    img = np.expand_dims(img, 0)
+
+    img = tf.keras.applications.resnet50.preprocess_input(img)
+   
+    extractedFeatures = MyModel.predict(img)
+        
+    extractedFeatures = np.array(extractedFeatures)
+
+    #THIS IS THE IMPORTANT ROW IN THIS PART
+    sk_struct['flattenPhoto'].append(extractedFeatures.flatten())
+    
+
+def ReadAndStoreMyImages(path):
+    """
+    Part1/2 of the basic setup used to process the images. 
+    It is called from main at the moment and sets up the looping
+    """
+
+    filenames_only = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+    i=0
+    for filename in filenames_only:
+
+        #THIS IS THE IMPORTANT ROW IN THIS PART
+        sk_struct['photo_name'].append(filename)
+        
+        imagePath=path/filename
+        filenames.append(imagePath)
+        LoadDataAndDoEssentials(imagePath, 1000, 1000)
+        '''
+        if i>1: 
+            break
+        else: i=i+1
+        '''
+     
+def splitting(c_X,c_path,c_unique_labels,c_labels):
+    count_start=0
+    for label in c_unique_labels:
+        new_x=filter_the_array(c_X,label,c_labels)
+        new_paths=filter_the_array(c_path,label,c_labels)
+        some_x, some_labels, some_paths=compute_dbscan_labels(new_x,new_paths)
+       
+        if -1 in some_labels:
+            some_labels=some_labels+1
+       
+        some_labels=some_labels+count_start
+        
+        if count_start==0:
+            prev_x=some_x
+            prev_label=some_labels
+            prev_path=some_paths
+        else:
+            prev_x=np.concatenate((prev_x,some_x),axis=0)
+            prev_label=np.concatenate((prev_label,some_labels),axis=0)
+            prev_path=np.concatenate((prev_path,some_paths),axis=0)
+        
+        count_start= max(some_labels)+1
+    
+    return (prev_x,prev_label, prev_path)
+        
+ 
+ 
+        
+def filter_the_array(x_val, filter_val, filter_list):
+    return np.fromiter((x for (y,x) in enumerate(x_val) if filter_list[y]==filter_val),
+                      dtype = 'object') 
+
+
+
+def compute_dbscan_labels(object_x, path_x):
+    new_x = np.vstack(object_x[:]).astype(np.float64)
+    new_path = np.vstack(path_x[:]).astype(str)
+    
+    # Compute the KNN distance
+    knn.fit(new_x)
+    new_distances, new_indices = knn.kneighbors(new_x)
+
+    new_distances = np.sort(new_distances, axis=0)
+    new_distances = new_distances[:,1]
+
+    new_eps=new_distances.mean()
+
+    # Apply DBSCAN
+    new_dbscan = DBSCAN(eps=new_eps, min_samples=5)
+    new_dbscan.fit(new_x)
+
+    new_labels = new_dbscan.labels_
+    new_unique_labels=set(new_labels)
+    #print(new_unique_labels)
+    if len(new_unique_labels) != 1:
+        return splitting(new_x,new_path,new_unique_labels,new_labels)
+    else:
+        return (new_x, new_labels,new_path)
+ 
+
+
+
+
+CURRENT_PATH_CWD = Path.cwd()
+TRAINING_DATA_DIR = CURRENT_PATH_CWD/'DATA'
+data_path=TRAINING_DATA_DIR
+TRAINING_DATA_DIR = TRAINING_DATA_DIR/'TRAIN'
+TRAINING_DATA_DIR = TRAINING_DATA_DIR/'TRAINING_DATA'
+
+filenames=[]
+ReadAndStoreMyImages(TRAINING_DATA_DIR)
+
+
+######################################################
+#        lets now do clustering                      #
+######################################################
+
+X = np.array(sk_struct['flattenPhoto'], dtype = 'float64')
+
+
+#print("shape of X")
+#print (X.shape)
+
+# Compute the KNN distance
+knn = NearestNeighbors(n_neighbors=2)
+knn.fit(X)
+distances, indices = knn.kneighbors(X)
+#print("shape of distances")
+#print(distances.shape)
+#print ("shape of indices")
+#print(indices.shape)
+
+distances = np.sort(distances, axis=0)
+distances = distances[:,1]
+#print("shape of distances2")
+#print(distances.shape)
+
+eps=distances.mean()
+
+# Apply DBSCAN
+dbscan = DBSCAN(eps=eps, min_samples=5)
+dbscan.fit(X)
+
+#print("shape of dbscan")
+#print(dbscan.labels_.shape)
+
+labels = dbscan.labels_
+unique_labels=set(labels)
+
+
+# Count the number of clusters
+num_clusters = len(unique_labels) - (1 if -1 in unique_labels else 0)
+#print('Number of clusters:', num_clusters)
+
+data_value, label_value,filename_value=splitting(X,filenames,unique_labels,labels)
+
+
+
+for label in set(label_value):
+    new_path=data_path/str(label)
+    if new_path not in data_path.glob("*"):
+        os.mkdir(new_path)
+
+#print("filenames", type(filenames), "structure", len(filenames))
+#print("filename_value", type(filename_value), "structure", filename_value.shape,"other", filename_value[0], "+", type(filename_value[0]))
+
+# copies images in coresponding folder
+for filename, label in zip(filename_value, label_value):
+    #print(label, type(label))
+    #print(filename, type(filename))
+    path=data_path/str(label)
+    #print (path, type(path))
+    shutil.copy(str(filename[0]),path)
---
+++ README.md
@@ -0,0 +1,11 @@
+# TENSORFLOW-TEST
+it is meant to do the following
+1. take some images of lines and create clusters and thereby classes according to the shape similarity
+2. be able to be fed in input other new images which to class it apartains
+
+for point 1 we used tensorflow keras to create a RESNET model which we ulterior processed using scikit learn k nearest neighbours and dbscan to cluster. Based on the prediction from these we classified the images.
+
+to function create a venv in which to install the requirements under python 3.10.6
+to be noted tensorflow is with cuda but if your configuration doesn't allow it you might either revert to 2.14 or rebuild a custom build for gpu usage
+
+the file test.py is there just to check if your tensorflow is gpu enabled
